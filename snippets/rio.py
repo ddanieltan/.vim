2c6d11db692f8a53cd699450c564d17181e22b82
Daniel Tan <dtan@tripadvisor.com>
2018-03-19 11:39:29 +0800

    moving in ipython snippets into this repo

---
+++ snippets/rio.py
@@ -0,0 +1,22 @@
+# Load usual np/pd libraries
+
+import numpy as np
+import pandas as pd
+
+# Load SQL magics
+import sqlalchemy
+import psycopg2
+import os
+
+connect_to_db='postgresql+psycopg2://{}:{}@{}:{}/{}'.format(os.environ.get('Rio_DBUSER'),
+                                                            os.environ.get('Rio_DBPASS'),
+                                                                                                                              os.environ.get('Rio_DBHOST'),
+                                                                                                                                                                                         os.environ.get('Rio_DBPORT'),
+                                                                                                                                                                                                                                                    os.environ.get('Rio_DBNAME'))
+
+vefe, gege ge
+geg,  ge, gec
+
+%load_ext sql
+%config SqlMagic.displaylimit=10
+%sql $connect_to_db
